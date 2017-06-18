042008ae31be3b8b12778f538eff6f63038738b5
dimiturtrz <dimitar.trz@gmail.com>
2017-06-18 10:07:41 +0300

    set up the rows for cells to appear

--- Row.cpp
+++ Row.cpp
@@ -2,9 +2,44 @@
 
 Row::Row() {}
 Row::Row(char* rowStr) {
-	
+	char* reader = rowStr;
+	char buffer[1024];
+	int offset = 0;
+	while(readCellStr(reader, buffer, &offset)) {
+		reader += offset;
+		//cells.add(buffer);
+	}
 }
 
-void Row::writeToStream(std::fstream& stream) {}
-void Row::print() {}
-void Row::setCell(int col, const char* newContent) {}
+void Row::writeToStream(std::fstream& stream) {
+	/*
+	for(int i=0; i< cells.getSize(); ++i) {
+		cells[i].writeToStream(stream);
+	}
+	*/
+}
+void Row::print() {
+	/*
+	for(int i=0; i< cells.getSize(); ++i) {
+		cells[i].print();
+	}
+	std::cout<< std::endl;
+	*/
+}
+void Row::setCell(int col, const char* newContent) {
+	/*
+		cells[col] = Cell(newContent);
+	*/
+}
+
+
+//----------------------- ROW STRING METHODS -------------------
+bool Row::readCellStr(const char* start, char* buffer, int* len) {
+	int i = 0;
+	for(; start[i] != ',' && start[i] != '\0'; ++i) {
+		buffer[i] = start[i];
+	}
+	buffer[i] = '\0';
+	*len = ++i;
+	return start[i] != '\0';
+}
--- Row.h
+++ Row.h
@@ -14,6 +14,9 @@ public:
 	void writeToStream(std::fstream& stream);
 	void print();
 	void setCell(int col, const char* newContent);
+
+private:
+	bool readCellStr(const char* start, char* buffer, int* length = NULL);
 };
 
 #endif
